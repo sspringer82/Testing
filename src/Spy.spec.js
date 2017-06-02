91c25c862a2d6a73ddfd23078a23c2c28fb201c0
Sebastian Springer <info@sebastian-springer.com>
2017-06-02 13:55:12 +0200

    spy and call through

--- src/Spy.spec.js
+++ src/Spy.spec.js
@@ -2,9 +2,25 @@ describe('Spy', function () {
   it('should record all calls', function () {
     const spy = jasmine.createSpy();
 
-    spy(1, 2, 3);
+    execute(spy);
 
-    expect(spy).toHaveBeenCalled();
-    expect(spy).toHaveBeenCalledWith(1, 2, 3);
+    //expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledWith('Hello World');
   });
+
+  it('should call the original function', function () {
+
+    const obj = {
+      callback(value) {
+        expect(value).toBe('Hello World');
+      }
+    }
+
+    spyOn(obj, 'callback').and.callThrough();
+
+    execute(obj.callback);
+
+    expect(obj.callback).toHaveBeenCalled();
+  })
+
 });
