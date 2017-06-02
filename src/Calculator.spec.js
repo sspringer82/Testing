918449e5d97ceb8fc320c57edc8cd1129ae02cc5
Sebastian Springer <info@sebastian-springer.com>
2017-06-02 11:42:16 +0200

    hooks

--- src/Calculator.spec.js
+++ src/Calculator.spec.js
@@ -1,9 +1,28 @@
 describe('Calc', function () {
+  let calc;
+
+  // sparsam benutzen
+  beforeAll(function () {
+    console.log('beforeAll');
+  });
+  afterAll(function () {
+    console.log('afterAll');
+  });
+
+  // häufig nutzen für initialisierung
+  beforeEach(function () {
+    console.log('beforeEach');
+    calc = new Calc();
+  });
+
+  // häufig nutzen für aufräumen
+  afterEach(function () {
+    console.log('afterEach');
+  })
 
   it ('should create an instance of calc', function () {
+    console.log('in my test');
     // arrange
-    const calc = new Calc();
-
     // act
     const result = calc instanceof Calc;
 
@@ -11,8 +30,17 @@ describe('Calc', function () {
     expect(result).toBe(true);
   });
 
+
   it('should add 1 and 1 and return 2', function () {
+    const result = calc.add(1, 1);
+
+    expect(result).toBe(2);
+  })
+
+  it('should add 2 and 3 and return 5', function () {
+    const result = calc.add(2, 3);
     
+    expect(result).toBe(5);
   })
 
 });
