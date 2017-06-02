d231fb449944070c633b817317262dd6bae96d66
Sebastian Springer <info@sebastian-springer.com>
2017-06-02 14:26:54 +0200

    logger 2

--- src/Logger.js
+++ src/Logger.js
@@ -2,6 +2,8 @@ class Logger {
   log(msg, sev) {
     if (sev === 'info') {
       console.log(msg);
+    } else if (sev === 'error') {
+      console.error(msg);
     }
   }
 }
--- src/Logger.spec.js
+++ src/Logger.spec.js
@@ -1,10 +1,24 @@
 describe('Logger', function () {
-  it('should log an info message if severety is info', function () {
-    const logger = new Logger();
-    spyOn(console, 'log');
 
+  let logger;
+
+  beforeEach(function () {
+    logger = new Logger();
+    spyOn(console, 'error');
+    spyOn(console, 'log');
+  });
+
+  it('should log an info message if severety is info', function () {
     logger.log('Msg', 'info');
 
     expect(console.log).toHaveBeenCalledWith('Msg');
+    expect(console.error).not.toHaveBeenCalled();
+  });
+
+  it('should log an error message if severety is error', function () {
+    logger.log('Msg', 'error');
+
+    expect(console.error).toHaveBeenCalledWith('Msg');
+    expect(console.log).not.toHaveBeenCalled();
   });
 });
